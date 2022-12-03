5004f4d8a24dd1299e45e57d05d24da7b8ce5fca
chiragramani <chirag.ramani7@gmail.com>
2022-12-02 16:07:18 -0800

    Initial commit

---
+++ Sources/FlipperLitePeertalk/include/FlipperLitePeertalk.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlipperLitePeertalk.
+FOUNDATION_EXPORT double FlipperLitePeertalkVersionNumber;
+
+//! Project version string for FlipperLitePeertalk.
+FOUNDATION_EXPORT const unsigned char FlipperLitePeertalkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlipperLitePeertalk/PublicHeader.h>
+
+#import <../PTChannel.h>
+#import <../PTProtocol.h>
+#import <../PTUSBHub.h>
+
